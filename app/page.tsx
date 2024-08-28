2343a842235ac1fdc84b07f9ca767e343df8590b
Andrew Abbey <andrewabbey@MacBook-Pro-6.attlocal.net>
2024-08-28 16:26:46 -0400

    Update homepage with IAN bot message

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold text-center">
+        IAN is a BOT, Beep boop
+      </h1>
+      <p className="mt-4 text-xl">
+        REBO.CO COMING SOON
+      </p>
+    </main>
+  );
+}
